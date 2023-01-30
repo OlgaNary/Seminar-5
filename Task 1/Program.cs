c8d16f183f349a5a4e749488f1517c882ed3e836
Ольга <olga@mail.ru>
2023-01-30 21:04:50 +0300

    Task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,42 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] arr = GetRandomArray(5);
+PrintArray(arr);
+System.Console.Write($"{GetSumEvenNum(arr)} четных чисел.");
+
+// Метод создает рандомный одномерный массив трёхзначных положительных чисел:
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
+
+// Метод выводит на печать заданный массив:
+void PrintArray(int[] arr)
+{
+    System.Console.Write("В массиве: ");
+    foreach (int item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    System.Console.WriteLine();
+}
+
+//Метод считает количество четных чисел в массиве:
+int GetSumEvenNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
